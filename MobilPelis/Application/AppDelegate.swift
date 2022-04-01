58e4d9f5811b8d5522cde2cc17af9d0ce783eee0
Sebastian Soto Varas <rsotov1@outlook.com>
2022-04-01 14:32:12 -0500

    Setup project MVVM and add initial login module

---
+++ MobilPelis/Application/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MobilPelis
+//
+//  Created by Sebastian Soto Varas on 1/04/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MobilPelis' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MobilPelis
+
+  target 'MobilPelisTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MobilPelisUITests' do
+    # Pods for testing
+  end
+
+end
