5f8ab394802e24cae82084d6ede363a2054369f1
Sebastian Soto Varas <rsotov1@outlook.com>
2022-04-02 19:49:15 -0500

    Add module login

---
+++ MobilPelis/Model/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  MobilPelis
+//
+//  Created by Sebastian Soto Varas on 2/04/22.
+//
+
+import Foundation
+
+struct UserModel: Decodable {
+    let username: String
+    let password: String
+}
--- MobilPelis/View/Login/LoginViewController.swift
+++ MobilPelis/View/Login/LoginViewController.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var LoginButton: UIButton!
+    private var loginVM = LoginViewModel()
+    
+    @IBAction func loginAction(_ sender: Any) {
+        validateCredentials()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupLoginView()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupLoginView() {
+        LoginButton.layer.cornerRadius = 10
+        LoginButton.layer.masksToBounds = true
     }
-    */
-
+    
+    private func validateCredentials() {
+        guard let username = usernameTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        loginVM.validateFields(username, password)
+        loginVM.loginCompletationHandler { (status, message) in
+            if status {
+                self.showToast(message: message, font: .systemFont(ofSize: 12.0))
+            } else {
+                self.showToast(message: message, font: .systemFont(ofSize: 12.0))
+            }
+        }
+    }
 }
